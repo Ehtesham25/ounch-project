8e639f38c60b2b7340f380175b29d321f1f93357
ehtesham25 <ehteshamulhaqkhan25@gmail.com>
2024-07-17 17:29:29 +0500

    refactor the code

--- pages/api/items.js
+++ pages/api/items.js
@@ -1,10 +1,10 @@
-import db from '../../lib/db';
+import db from "../../lib/db";
 
 export default async function handler(req, res) {
-    try {
-        const [rows] = await db.query('SELECT * FROM items');
-        res.status(200).json(rows);
-    } catch (error) {
-        res.status(500).json({ error: 'Error fetching data' });
-    }
+  try {
+    const [rows] = await db.query("SELECT * FROM items");
+    res.status(200).json(rows);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching data" });
+  }
 }
